9ac954ba80cab286dadefd851413b77c850dbbdd
Aditya Dubey <fordevelopmentusecase@gmail.com>
2026-08-20 19:39:08 +0530

    feat: implement token refresh endpoint and authenticated fetch wrapper for automatic session renewal

---
+++ app/api/v1/auth/refresh/refresh.ts
@@ -0,0 +1,58 @@
+import { errorResponse, successResponse } from "@/lib/apiResponse";
+import { connectDB } from "@/lib/db";
+import { env } from "@/lib/env";
+import { signAccessToken, signRefreshToken, verifyRefreshToken } from "@/lib/jwt";
+import { User } from "@/lib/models/User";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const refreshToken = request.cookies.get("refreshToken")?.value;
+
+    if (!refreshToken) {
+      return errorResponse("No refresh token", 401);
+    }
+
+    let payload;
+    try {
+      payload = verifyRefreshToken(refreshToken);
+    } catch {
+      return errorResponse("Invalid or expired refresh token", 401);
+    }
+
+    await connectDB();
+    const user = await User.findById(payload.userId);
+
+    if (!user) {
+      return errorResponse("User no longer exist", 401);
+    }
+
+    const tokenPayload = {
+      userId: user._id.toString(),
+    };
+    const newAccessToken = signAccessToken(tokenPayload);
+    const newRefreshToken = signRefreshToken(tokenPayload);
+
+    const response = successResponse({
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        avatarUrl: user.avatarUrl,
+      },
+      accessToken: newAccessToken,
+    });
+
+    response.cookies.set("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: env.NODE_ENV === "production",
+      path: "/",
+      maxAge: 7 * 24 * 60 * 60,
+      sameSite: "lax",
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Refresh error", error);
+    return errorResponse("Something went wrong");
+  }
+}
